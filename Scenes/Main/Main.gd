167b380e7762ab2355eda5bf1d7e7ae03907941c
dayton-bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2020-03-18 19:38:14 -0400

    feat: music and sounds

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -12,12 +12,15 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 
 func new_game():
 	score = 0
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 
