50046a53aa006853fc4654593b0293865a0ca704
dayton-bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2020-03-18 19:30:11 -0400

    feat: control spawn location from Main scene

--- Scenes/Mob/Mob.gd
+++ Scenes/Mob/Mob.gd
@@ -1,7 +1,5 @@
 extends RigidBody2D
 
-enum SpawnLocation {TOP, BOTTOM, LEFT, RIGHT}
-
 var screen_size: Vector2
 
 # mob speed in pixels/second
@@ -15,49 +13,35 @@ var direction: Vector2
 # offset used to ensure the mob spawns off-screen
 var offset: float
 
+
 func _ready():
 	hide()
 	screen_size = get_viewport_rect().size
 	offset = $CollisionShape2D.shape.radius * 4
 	speed = randi() % max_speed + min_speed
-	$VisibilityNotifier2D.connect("screen_entered", self, "_on_Visibility_screen_entered")
-	$VisibilityNotifier2D.connect("screen_exited", self, "_on_Visibility_screen_exited")
+
 
 func _process(delta):
 	position += direction * speed * delta
 
+
+func _on_start_game():
+	queue_free()
+
+
 func _on_Visibility_screen_entered():
 	$AnimatedSprite.play()
+
 
 func _on_Visibility_screen_exited():
 	queue_free()
 
+
 # spawn mob at a random position and move towards player
-func spawn(player_position: Vector2):
-	var spawn_location = _get_spawn_location()
-	position = _get_random_position(spawn_location)
+func spawn(spawn_location: Vector2, player_position: Vector2):
+	position = spawn_location
 	direction = position.direction_to(player_position)
 	
 	# rotate mob to face the player
 	rotation = direction.angle()
 	show()
-
-func _get_spawn_location():
-	return SpawnLocation.values()[randi() % SpawnLocation.size()]
-
-func _get_random_position(spawn_location: int):
-	var random_position = Vector2()
-	match spawn_location:
-		SpawnLocation.TOP:
-			random_position.x = randi() % (screen_size.x as int)
-			random_position.y = -offset
-		SpawnLocation.BOTTOM:
-			random_position.x = randi() % (screen_size.x as int)
-			random_position.y = screen_size.y + offset
-		SpawnLocation.LEFT:
-			random_position.x = -offset
-			random_position.y = randi() % (screen_size.y as int)
-		SpawnLocation.RIGHT:
-			random_position.x = screen_size.x + offset
-			random_position.y = randi() % (screen_size.y as int)
-	return random_position
